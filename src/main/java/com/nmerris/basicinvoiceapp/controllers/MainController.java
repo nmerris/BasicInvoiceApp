16945c79edd1ae9182b461041a455c5826d0f05d
nmerris <nate.merris@gmail.com>
2017-08-02 11:06:45 -0400

    initial commit

---
+++ src/main/java/com/nmerris/basicinvoiceapp/controllers/MainController.java
@@ -0,0 +1,42 @@
+package com.nmerris.basicinvoiceapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("/index")
+    public @ResponseBody String indexPage() {
+        return "<h1>Welcome to the basic invoice web app<h1>";
+    }
+
+
+    @GetMapping("/login")
+    public @ResponseBody String loginPage() {
+        return "This is the login page";
+    }
+
+
+    @GetMapping("/addproduct")
+    public @ResponseBody String addProduct() {
+        return "Add a product here";
+    }
+
+
+    @RequestMapping("/listproducts")
+    public String listProducts(Model model) {
+        String dependecyList = "The dependencies used are: spring-boot-starter-web, spring-boot-starter-test, spring-boot-starter-thymeleaf";
+
+        model.addAttribute("productlistheading", "This is a list of your products");
+        model.addAttribute("dependencylist", dependecyList);
+
+        return "listproducts";
+    }
+
+
+}
---
+++ src/test/java/com/nmerris/basicinvoiceapp/BasicinvoiceappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nmerris.basicinvoiceapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicinvoiceappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
